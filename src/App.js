ef1f4c0c2a9f763926f3b6b0be13da817be4e155
DivyaShree <divya4545shree@gmail.com>
2024-11-06 11:15:15 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import login from './login';
-
+import React from 'react';
 import './App.css';
 import Login from './Components/Login';
 
---
+++ src/function Component/Signup.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './style.css'
+
+function Signup  () {
+  return (
+    <div className='container'>
+      <div className='form_container'>
+        <div className='form'>
+             <h2> Create an Account</h2>
+             <div className='container'>
+              <label><br>Company email</br></label>
+              <input type='text' placeholder='Email' name='name'/>
+             </div>
+        </div>
+
+      </div>
+
+    </div>
+    
+  
+ 
+
+  )    
+} 
+export default Signup
+
+
+
+
